e74a12a5dc45fd93027e049bc9279d6ddde3f578
Abroor Rizky <abroorrizky@gmail.com>
2025-06-09 14:16:36 +0700

    First Commit

---
+++ database/migrations/2025_05_29_061804_add_role_id_photo_and_nip.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('role_id')->after('id')->nullable();
+            $table->string('photo')->after('email')->nullable();
+            $table->string('nip')->after('photo')->nullable();
+
+            // Add foreign key constraint
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('restrict')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
